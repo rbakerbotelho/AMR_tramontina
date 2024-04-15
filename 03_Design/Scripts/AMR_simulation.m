52466da5f27c8451ecac67bbdc9672b9aa655bc9
bordina <anderson.bordin@tramontina.com>
2024-04-15 11:04:37 -0300

    add AMR_simulation script to model param initialization

---
+++ 03_Design/Scripts/AMR_simulation.m
@@ -0,0 +1,55 @@
+clear('variables');
+close('all');
+clc;
+
+% Add os paths dos modelos
+filename='AMR_main_24a';
+%addpath('.\03_Design\Models\');
+%folder = fileparts(which('AMR_main_24a')); 
+% addpath(genpath(folder));
+addpath(genpath(fileparts( which(filename) )));
+
+% rotas EXTERNAS pré-cadastradas
+% T1 = readtable('.\stateFlow\current\routes\route1.csv');
+% T2 = readtable('.\stateFlow\current\routes\route2.csv');
+% T3 = readtable('.\stateFlow\current\routes\route3.csv');
+% T4 = readtable('.\stateFlow\current\routes\route4.csv');
+T5 = readtable(which('route5.csv'));
+T6 = readtable(which('route6.csv')); % 'T' atrás tintas
+T7 = readtable(which('route7.csv')); % Linha Reta 239
+
+T=T6;
+
+%% Define os parâmetros de simulação
+num_of_wps = size(T, 1);
+tfinal = inf;
+L = 2;
+
+% % Abre o modelo
+% % open_system('.\models\AGV_stateFlow.slx');
+% 
+% % Inicia o servidor web para receber dados do AGV
+% % Executar uma unica vez o comando abaixo para abrir a interface web
+% % system("start cmd /k python web_interface.py");
+% 
+% % Simula
+% S = sim('.\models\AGV_stateFlow.slx');
+% 
+% %% Separa as variáveis observadas
+% AGV_t = S.logsout{1}.Values.Time;
+% AGV_lat = S.logsout{1}.Values.Data(:);
+% AGV_lng = S.logsout{2}.Values .Data(:);
+% AGV_heading = S.logsout{3}.Values.Data(:);
+% AGV_speed = S.logsout{4}.Values.Data(:);
+% AGV_moving = S.logsout{5}.Values.Data(:);
+% AGV_state = S.logsout{6}.Values.Data(:);
+% 
+% % Geoplot Figura total
+% figure('color','w');
+% geoplot(T1.Latitude,T1.Longitude,'b-*','LineWidth',2);
+% hold('on');
+% geoplot(AGV_lat,AGV_lng,'r-','LineWidth',2)
+% geobasemap satellite;
+% 
+% % Faz um video
+% % run('scripts\create_video.m');
