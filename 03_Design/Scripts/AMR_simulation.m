5300af8c5835832c0ff9bd120cfccd71197d3870
bordina <96056313+bordina@users.noreply.github.com>
2024-05-27 05:48:26 -0300

    added (unfinished) models to path simulation

--- 03_Design/Scripts/AMR_simulation.m
+++ 03_Design/Scripts/AMR_simulation.m
@@ -17,8 +17,9 @@ T=T_ind0;
 
 %% Define os parâmetros de SIMULAÇÃO
 num_of_wps = size(T, 1);
+tstep = 50e-3;
 tfinal = 500;
-L = 0.25;
+L = 2;
 
 %% SIMUL param
 % Abre o modelo
@@ -26,26 +27,63 @@ open_system(model);
 
 %Inicia o servidor web para receber dados do AGV
 % Executar uma unica vez o comando abaixo para abrir a interface web
-system("start cmd /k python web_interface.py");
+%system("start cmd /k python web_interface.py");
 
 % Simula
 S = sim(model);
 
 % Separa as variáveis observadas
 AMR_t = S.logsout{1}.Values.Time;
+AMR_x = S.logsout{3}.Values.Data(:);
+AMR_y = S.logsout{4}.Values .Data(:);
+AMR_heading = S.logsout{7}.Values.Data(:);
+AMR_speed = S.logsout{6}.Values.Data(:);
+AMR_moving = S.logsout{15}.Values.Data(:);
+AMR_state = S.logsout{14}.Values.Data(:);
+route_x = S.logsout{21}.Values.Data(:);
+route_y = S.logsout{22}.Values.Data(:);
+
+
+p=plot(route_x, route_y,'-o');
+p.MarkerSize = 5;
+hold on
+plot(AMR_x, AMR_y);
+legend('target','executed')
+grid minor 
+
+
+
+
+
+model='C:\Users\ander\Desktop\AGV_stateFlow22a.slx';
+S = sim(model);
+
+% %% Separa as variáveis observadas
+AGV_t = S.logsout{1}.Values.Time;
 AGV_lat = S.logsout{1}.Values.Data(:);
 AGV_lng = S.logsout{2}.Values .Data(:);
-AGV_heading = S.logsout{3}.Values.Data(:);
-AGV_speed = S.logsout{4}.Values.Data(:);
-AGV_moving = S.logsout{5}.Values.Data(:);
-AGV_state = S.logsout{6}.Values.Data(:);
+AGV_speed = S.logsout{3}.Values.Data(:);
+AGV_heading = S.logsout{4}.Values.Data(:);
+AGV_moving = S.logsout{11}.Values.Data(:);
+AGV_state = S.logsout{12}.Values.Data(:);
+AGV_X = S.logsout{14}.Values.Data(:);
+AGV_Y = S.logsout{15}.Values.Data(:);
 
-% Geoplot Figura total
-figure('color','w');
-geoplot(T1.Latitude,T1.Longitude,'b-*','LineWidth',2);
-hold('on');
-geoplot(AGV_lat,AGV_lng,'r-','LineWidth',2)
-geobasemap satellite;
+
+p=plot(AGV_X, AGV_Y,'-o');
+p.MarkerSize = 5;
+hold on
+plot(AGV_lat, AGV_lng)
+legend('target','executed')
+grid minor 
+
+
+% % Geoplot Figura total
+% figure('color','w');
+% geoplot(T1.Latitude,T1.Longitude,'b-*','LineWidth',2);
+% hold('on');
+% geoplot(AGV_lat,AGV_lng,'r-','LineWidth',2)
+% geobasemap satellite;
 
 % Faz um video
 % run('scripts\create_video.m');
